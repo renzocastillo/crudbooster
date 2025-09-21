f46a1c2fca4a7f5c141d559f8a1549deeb062524
Renzo Castillo <renzo.castillo@quaira.com>
2025-09-21 18:01:38 -0500

    wip

--- src/controllers/CBController.php
+++ src/controllers/CBController.php
@@ -541,7 +541,7 @@ class CBController extends Controller
         }
 
         $mainpath = CRUDBooster::mainpath();
-        $orig_mainpath = $mainpath;
+        $orig_mainpath = $this->data['mainpath'];
         $title_field = $this->title_field;
         $html_contents = [];
         $page = (request('page')) ? request('page') : 1;
